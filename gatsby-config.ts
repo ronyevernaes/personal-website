8964e1c22d02d9ccd521511706d231a2b8a36c58
Ronye Vernaes <remnahush@gmail.com>
2024-11-28 08:30:13 +0100

    add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -33,7 +33,25 @@ const config: GatsbyConfig = {
       },
       __key: "pages"
     },
-    "gatsby-plugin-provide-react"
+    "gatsby-plugin-provide-react",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Ronye Vernaes",
+        short_name: "Ronye Vernaes",
+        start_url: "/",
+        background_color: "#1f2937",
+        theme_color: "#be123c",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
 
